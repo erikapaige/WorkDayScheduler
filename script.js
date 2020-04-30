db406e61a852d0820c65e9d5cc8714738828ca8f
Erika Paige <erika.paige.handley@gmail.com>
2020-04-29 21:41:03 -0700

    outline of process

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-var a = moment().formant()
-console.log(a)
+// add the date below work day scheduler, that needs to update to current
+
+// create blocks of time from 9 AM - 6 PM
+  //3 columns with 9 rows
+
+// color code meetings
+
+// add new meetings to each time block(like the to do list)
+
+// save each timeblock to local storage
