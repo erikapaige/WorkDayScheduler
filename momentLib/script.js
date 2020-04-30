d892b138dba0005a9d9fc44489e3e6aeca4778a8
Erika Paige <erika.paige.handley@gmail.com>
2020-04-29 23:05:17 -0700

    added current date to calendar

--- momentLib/script.js
+++ momentLib/script.js
@@ -1,4 +1,5 @@
-// add the date below work day scheduler, that needs to update to current
+// add the date below work day scheduler, that needs to update to current day
+document.getElementById('currentDay').innerHTML = moment().format('dddd MMMM Do')
 
 // create blocks of time from 9 AM - 6 PM
   //3 columns with 9 rows
