a03be7b0f82964114bd24a20c0439df5013ae737
Erika Paige <erika.paige.handley@gmail.com>
2020-04-30 21:30:58 -0700

    have event listener working in script tag

--- momentLib/script.js
+++ momentLib/script.js
@@ -3,92 +3,86 @@ document.getElementById('currentDay').innerHTML = moment().format('dddd MMMM Do'
 
 //variable establishing current time in military time
 const currentTime = (moment().format('HH'))
-
+console.log(currentTime)
 // conditional statements checking each block of time and adding the relevant color
-const nineAM = document.getElementById('hr9')
-if (nineAM === currentTime) {
+// const nineAM = document.getElementById('hr9')
+if (currentTime == '09') {
   document.getElementById('hr9').style.background = 'rgb(255, 105, 97)'
-} else if (nineAM < currentTime) {
+} else if ('09' < currentTime) {
   document.getElementById('hr9').style.background = 'rgb(211, 211, 211)'
 } else {
   document.getElementById('hr9').style.background = 'rgb(119, 221, 119)'
 }
 
-const tenAM = document.getElementById('hr10')
-if (tenAM === currentTime) {
+if (currentTime == '10') {
   document.getElementById('hr10').style.background = 'rgb(255, 105, 97)'
-} else if (tenAM < currentTime) {
+} else if ('10' < currentTime) {
   document.getElementById('hr10').style.background = 'rgb(211, 211, 211)'
 } else {
   document.getElementById('hr10').style.background = 'rgb(119, 221, 119)'
 }
 
-const elevenAM = document.getElementById('hr11')
-if (elevenAM === currentTime) {
+if (currentTime == '11') {
   document.getElementById('hr11').style.background = 'rgb(255, 105, 97)'
-} else if (elevenAM < currentTime) {
+} else if ('11' < currentTime) {
   document.getElementById('hr11').style.background = 'rgb(211, 211, 211)'
 } else {
   document.getElementById('hr11').style.background = 'rgb(119, 221, 119)'
 }
 
-const twelvePM = document.getElementById('hr12')
-if (twelvePM === currentTime) {
+if (currentTime == '12') {
   document.getElementById('hr12').style.background = 'rgb(255, 105, 97)'
-} else if (twelvePM < currentTime) {
+} else if ('12' < currentTime) {
   document.getElementById('hr12').style.background = 'rgb(211, 211, 211)'
 } else {
   document.getElementById('hr12').style.background = 'rgb(119, 221, 119)'
 }
 
-const onePM = document.getElementById('hr13')
-if (onePM === currentTime) {
+if (currentTime == '13') {
   document.getElementById('hr13').style.background = 'rgb(255, 105, 97)'
-} else if (onePM < currentTime) {
+} else if ('13' < currentTime) {
   document.getElementById('hr13').style.background = 'rgb(211, 211, 211)'
 } else {
   document.getElementById('hr13').style.background = 'rgb(119, 221, 119)'
 }
 
-const twoPM = document.getElementById('hr14')
-if (twoPM === currentTime) {
+if (currentTime == '14') {
   document.getElementById('hr14').style.background = 'rgb(255, 105, 97)'
-} else if (twoPM < currentTime) {
+} else if ('14' < currentTime) {
   document.getElementById('hr14').style.background = 'rgb(211, 211, 211)'
 } else {
   document.getElementById('hr14').style.background = 'rgb(119, 221, 119)'
 }
 
-const threePM = document.getElementById('hr15')
-if (threePM === currentTime) {
+if (currentTime == '15') {
   document.getElementById('hr15').style.background = 'rgb(255, 105, 97)'
-} else if (threePM < currentTime) {
+} else if ('15' < currentTime) {
   document.getElementById('hr15').style.background = 'rgb(211, 211, 211)'
 } else {
   document.getElementById('hr15').style.background = 'rgb(119, 221, 119)'
 }
 
-const fourPM = document.getElementById('hr16')
-if (fourPM === currentTime) {
+if (currentTime == '16') {
   document.getElementById('hr16').style.background = 'rgb(255, 105, 97)'
-} else if (fourPM < currentTime) {
+} else if ('16' < currentTime) {
   document.getElementById('hr16').style.background = 'rgb(211, 211, 211)'
 } else {
   document.getElementById('hr16').style.background = 'rgb(119, 221, 119)'
 }
 
-const fivePM = document.getElementById('hr17')
-if (fivePM === currentTime) {
+if (currentTime == '17') {
   document.getElementById('hr17').style.background = 'rgb(255, 105, 97)'
-} else if (fivePM < currentTime) {
+} else if ('17' < currentTime) {
   document.getElementById('hr17').style.background = 'rgb(211, 211, 211)'
 } else {
   document.getElementById('hr17').style.background = 'rgb(119, 221, 119)'
 }
 
-// add new meetings to each time block (like the to do list)
-document.getElementById("in9").addEventListener("click", '')
-console.log(hi)
-
-
+// add new meetings to each time block click on the box and add an input (textarea)
 // save each timeblock to local storage, save by clicking on far right column
+
+//input for 9 AM apppointment
+document.getElementById("saveApptNine").addEventListener ("click", function() {
+  let apptInputNine = document.getElementById("apptInputNine").value
+  localStorage.setItem("apptInputNine", apptInputNine)
+})
